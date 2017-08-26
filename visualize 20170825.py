856b86d3f6393d12b99300085ec7e7b95c91bc20
wahahab <wahahab11@gmail.com>
2017-08-26 15:55:47 +0800

    change color map of batch visualization

--- visualize 20170825.py
+++ visualize 20170825.py
@@ -17,31 +17,37 @@ false_X = [row for i, row in enumerate(X) if Y[i] <= 0.5]
 plt.ylim([-50, 200])
 plt.xlim([0, 250])
 # plt.show()
-# 'K31358_WI', 'W90030_WI', '564760_WI', 
+# 'K31358_WI', 'W90030_WI', '564760_WI',
 files = {'026725_WI', '182497_WI', '207904_WI', 'K31358_WI', 'W90030_WI', '564760_WI', '897461_WI', 'J09164_WI',  'U34660_WI', 'W88469_WI', 'W91419_WI', 'Y04407_WI'}
 
 for test in files:
 
     print(test)
     train = pd.read_csv(test + '.csv')
-    
+
     true_rows = train[train['ICP_condition'] == 1]
     false_rows = train[train['ICP_condition'] == 0]
     if len(true_rows) < N or len(false_rows) < N:
         print('X')
         continue
     else:
+        plt.clf()
         ax = plt.axes()
+        fig = plt.gcf()
         # plt.plot([row[0] for row in true_X], [row[1] for row in true_X], 'r,')
         # plt.plot([row[0] for row in false_X], [row[1] for row in false_X], 'b,')
         xx, yy = np.meshgrid(np.arange(-50, N, 1),
                              np.arange(-50, N, 1))
         Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
         Z = Z.reshape(xx.shape)
-        out = ax.contourf(xx, yy, Z)
+        out = ax.contourf(xx, yy, Z, cmap='Oranges')
 
-        true_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='w', s=30, marker='.', edgecolors='k')
-        false_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='B', s=30, marker='.', edgecolors='k')
+        # add color bar to illustrate meaning of contours
+        cbar = fig.colorbar(out, ticks=[0, .5, 1])
+        cbar.ax.set_title('Probability of bad condition (predict result)', rotation='-90', loc='right', x=3.5)
+        true_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='w', s=30, marker='.', edgecolors='k', label='BAD')
+        false_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='b', s=30, marker='.', edgecolors='k', label='GOOD')
+
         # plt.show()
         print('O')
 
