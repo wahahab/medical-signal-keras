febf715808277b8e0ae27f0f6c846062c5bec902
wahahab <wahahab11@gmail.com>
2017-10-18 14:10:17 +0800

    commit

--- train.py
+++ train.py
@@ -1,30 +1,112 @@
+import sys
+import keras
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from datetime import datetime
+from keras.utils import np_utils
+from common import precision, recall
+from sklearn.preprocessing import LabelEncoder
+# from datetime import datetime
+# from keras.utils.np_utils import to_categorical
 
+
+def train_and_save(X, Y, model_name):
+    model = Sequential()
+    model.add(Dense(64, input_dim=1, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+
+    # model.compile(loss='categorical_crossentropy',
+    #               optimizer='adam',
+    #               metrics=['accuracy', precision, recall])
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy', precision, recall])
+
+    model.fit(X, Y,
+              epochs=5,
+              batch_size=128)
+    # Save trained model
+    # model.save('models/model-%s.h5' % datetime.isoformat(datetime.now()))
+    model.save('models/%s.h5' % (model_name))
+    score = model.evaluate(X, Y, batch_size=128)
+    print
+    print 'Good-Bad model training scores: %s' % (score)
+    return model
 
 # Load training data
 train_df = pd.read_csv('train.csv')
-train_X = train_df.loc[:, ['WABPm', 'WICPm']].as_matrix()
-train_Y = train_df.loc[:, 'ICP_condition'].as_matrix()
+train_df = train_df.loc[train_df.Condition < 7, :]
+train_X = train_df.loc[:, ['WICPm']].as_matrix()
+train_Y = train_df.loc[:, 'Condition'].as_matrix()
+train_Y = train_Y <= 3
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(train_Y)
+encoded_Y = encoder.transform(train_Y)
+# convert integers to dummy variables (i.e. one hot encoded)
+dummy_y = np_utils.to_categorical(encoded_Y)
 
-model = Sequential()
-model.add(Dense(64, input_dim=2, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+model = train_and_save(train_X, dummy_y, 'WICPm alone')
+# perform testing
+test_df = pd.read_csv('test.csv' if len(sys.argv) < 2 else sys.argv[1])
+test_df = test_df.loc[test_df.ICP_condition < 7, :]
+test_X = test_df.loc[:, ['WICPm']].as_matrix()
+test_Y = test_df.loc[:, 'ICP_condition'].as_matrix()
+predict_Y = model.predict(test_X, batch_size=128)
+np.savetxt('binary-all-predict.txt', predict_Y)
+# # train impovement prediction model
+# good_mask = (train_Y <= 3)
+# train2_X = train_X[good_mask, :]
+# train2_Y = train_Y[good_mask]
+# Y = ((train2_Y == 1) | (train2_Y == 3))
+# train_and_save(train2_X, Y, 'improving-model')
+# # train deterioration prediction model
+# bad_mask = (train_Y > 3)
+# train3_X = train_X[bad_mask, :]
+# train3_Y = train_Y[bad_mask]
+# Y = ((train3_Y == 4) | (train3_Y == 6))
+# train_and_save(train3_X, Y, 'deteriorating-model')
+#
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+N = 250
 
-model.fit(train_X, train_Y,
-          epochs=20,
-          batch_size=128)
-# Save trained model
-model.save('models/model-%s.h5' % datetime.isoformat(datetime.now()))
-score = model.evaluate(train_X, train_Y, batch_size=128)
-print 'Train scores: loss => %s, acc =>%s.' % (score[0], score[1])
+ax = plt.axes()
+fig = plt.gcf()
+xx, yy = np.meshgrid(np.arange(-50, N, 1),
+                     np.arange(-50, N, 1))
+Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = np.argmax(Z, axis=1)
+Z = Z.reshape(xx.shape)
+# visualize improvement predict result
+# good_mask = Z < 0.5
+# improving_Z = improve_model.predict(np.c_[xx.ravel(), yy.ravel()])
+# improving_Z = improving_Z.reshape(xx.shape)
+# deteriorating_Z = deteriorate_model.predict(np.c_[xx.ravel(), yy.ravel()])
+# deteriorating_Z = deteriorating_Z.reshape(xx.shape)
+# deteriorating_Z[good_mask] = improving_Z[good_mask]
+out = ax.contourf(xx, yy, Z, cmap='tab20')
+# add color bar to illustrate meaning of contours
+# cbar = fig.colorbar(out, ticks=[0, .5, 1])
+# cbar.ax.set_title('Probability of bad condition (predict result)', rotation='-90', loc='right', x=3.5)
+# print train['Condition'].as_matrix()
+# train.sample(300).plot('WABPm', 'WICPm', ax=ax, kind='scatter', c=list(train['Condition']), cmap=plt.get_cmap('ocean'))
+# ture_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='w', s=30, marker='.', edgecolors='k', label='BAD')
+# false_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='b', s=30, marker='.', edgecolors='k', label='GOOD')
+# ax.legend(['BAD', 'GOOD'])
+ax.set_title('Model visualization')
+plt.show()
+
+
+
+
+
+
+
+
--- visualize.py
+++ visualize.py
@@ -1,34 +1,59 @@
 import keras
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+# import pandas as pd
+
+from common import precision, recall
 
 
 N = 250
-model = keras.models.load_model('model.h5')
-X = [[i, j] for i in range(N) for j in range(-50, N)]
-Y = model.predict(X)
-true_X = [row for i, row in enumerate(X) if Y[i] > 0.5]
-false_X = [row for i, row in enumerate(X) if Y[i] <= 0.5]
-plt.ylim([-50, 200])
-plt.xlim([0, 250])
+model = keras.models.load_model('./models/good-bad-model.h5',
+                                {
+                                    'precision': precision,
+                                    'recall': recall,
+                                })
+improve_model = keras.models.load_model('./models/improving-model.h5',
+                                {
+                                    'precision': precision,
+                                    'recall': recall,
+                                })
+deteriorate_model = keras.models.load_model('./models/deteriorating-model.h5',
+                                {
+                                    'precision': precision,
+                                    'recall': recall,
+                                })
+# X = [[i, j] for i in range(N) for j in range(-50, N)]
+# Y = model.predict(X)
+# Y = np.argmax(Y, axis=1)
+# # true_X = [row for i, row in enumerate(X) if Y[i] > 0.5]
+# # false_X = [row for i, row in enumerate(X) if Y[i] <= 0.5]
+# plt.ylim([-50, 200])
+# plt.xlim([0, 250])
 
-train = pd.read_csv('train.csv')
+# train = pd.read_csv('train.csv')
 
-ture_rows = train[train['ICP_condition'] == 1]
-false_rows = train[train['ICP_condition'] == 0]
 ax = plt.axes()
 fig = plt.gcf()
 xx, yy = np.meshgrid(np.arange(-50, N, 1),
                      np.arange(-50, N, 1))
 Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
-out = ax.contourf(xx, yy, Z, cmap='Oranges')
+# visualize improvement predict result
+good_mask = Z < 0.5
+improving_Z = improve_model.predict(np.c_[xx.ravel(), yy.ravel()])
+improving_Z = improving_Z.reshape(xx.shape)
+deteriorating_Z = deteriorate_model.predict(np.c_[xx.ravel(), yy.ravel()])
+deteriorating_Z = deteriorating_Z.reshape(xx.shape)
+deteriorating_Z[good_mask] = improving_Z[good_mask]
+out = ax.contourf(xx, yy, deteriorating_Z, cmap='Oranges')
 # add color bar to illustrate meaning of contours
-cbar = fig.colorbar(out, ticks=[0, .5, 1])
-cbar.ax.set_title('Probability of bad condition (predict result)', rotation='-90', loc='right', x=3.5)
-ture_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='w', s=30, marker='.', edgecolors='k', label='BAD')
-false_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='b', s=30, marker='.', edgecolors='k', label='GOOD')
-# ax.legend(['BAD', 'GOOD'])
+print out
+cbar = fig.colorbar(out, ticks=range(6))
+# cbar.ax.set_title('Probability of bad condition (predict result)', rotation='-90', loc='right', x=3.5)
+# print train['Condition'].as_matrix()
+# train.sample(300).plot('WABPm', 'WICPm', ax=ax, kind='scatter', c=list(train['Condition']), cmap=plt.get_cmap('ocean'))
+# ture_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='w', s=30, marker='.', edgecolors='k', label='BAD')
+# false_rows.sample(N).plot('WABPm', 'WICPm', 'scatter', ax=ax, color='b', s=30, marker='.', edgecolors='k', label='GOOD')
+# ax.legend(out)
 ax.set_title('Model visualization')
 plt.show()
